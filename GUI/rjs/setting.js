c303409f502c00d8758375f74fee914057d772cd
William Roque <william.aroque@gmail.com>
2022-07-31 12:02:12 -0600

    Fixed zoom-related issues.

--- GUI/rjs/setting.js
+++ GUI/rjs/setting.js
@@ -28,6 +28,11 @@ function resizeCanvas(_, rerender=true) {
     penPreviewCanvas.style.width = compensateViewport(10) + 'vw';
     penPreviewCanvas.style.height = compensateViewport(10) + 'vw';
 
+    currentPenLabel.style.top = compensateViewport(4) + 'vw';
+    currentPenLabel.style.right = compensateViewport(5) + 'vw';
+
+    currentPenLabel.style.fontSize = compensateViewport(2.6) + 'vw';
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
@@ -53,6 +58,7 @@ function resizeCanvas(_, rerender=true) {
 
 function toggleBackground() {
     document.body.classList.toggle('light-body');
+    document.documentElement.classList.toggle('light-body');
     Properties.isLightMode = !Properties.isLightMode;
 
     currentPen.brush.nextColor();
@@ -67,6 +73,7 @@ function setBackground(background) {
 
 function setLightMode() {
     document.body.classList.add('light-body');
+    document.documentElement.classList.add('light-body');
     Properties.isLightMode = true;
 
     currentPen.brush.nextColor();
@@ -76,6 +83,7 @@ function setLightMode() {
 
 function setDarkMode() {
     document.body.classList.remove('light-body');
+    document.documentElement.classList.remove('light-body');
     Properties.isLightMode = false;
 
     currentPen.brush.nextColor();
